c4604c0e0293d33bcc3757a44bde23339b93df51
markalbino <32577581+markalbino@users.noreply.github.com>
2017-10-06 19:00:01 -0300

    Carro.java

---
+++ Carro.java
@@ -0,0 +1,71 @@
+package primeiraaplicacao;
+
+
+
+public class Carro {
+
+//atributos
+
+    
+
+    private String chassi;
+
+    private String cor;
+
+    private String modelo;    
+
+
+
+    //Getters and Setters
+
+    
+
+        public void setChassi(String chassi){
+
+        
+
+            this.chassi= chassi;
+
+ }
+
+        public String getChassi(){
+
+        
+
+            return chassi;
+
+ }
+
+        public void setCor(String cor){
+
+        
+
+            this.cor= cor;
+
+ }
+
+        public String getcor(){
+
+        
+
+            return cor;
+
+ } 
+
+        public void setModelo(String modelo){
+
+        
+
+            this.modelo= modelo;
+
+ }
+
+        public String getmodelo(){
+
+        
+
+            return modelo;
+
+ }
+
+ }
