8ce6d2bd42c35307eba25a44ac6cf4090d4cd501
markalbino <32577581+markalbino@users.noreply.github.com>
2017-10-18 23:54:56 -0200

    IngressoNormal.java

---
+++ IngressoNormal.java
@@ -0,0 +1,10 @@
+package ingresso;
+
+public class IngressoNormal extends Ingresso {
+
+    private final double valor = valorIngresso = 100;
+
+    public String IngressoNormal() {
+        return "Ingresso normal: " + valor;
+    }
+}
