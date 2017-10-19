22ccac0965c38bb118b0652e3ab2ca0e0954acf3
markalbino <32577581+markalbino@users.noreply.github.com>
2017-10-18 23:49:30 -0200

    NumeroDecrescente.java

---
+++ NumeroDecrescente.java
@@ -0,0 +1,24 @@
+package numerodecrescente;
+
+import java.util.Scanner;
+
+public class NumeroDecrescente {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite um numero:");
+        int num1 = sc.nextInt();
+
+        int vet[] = new int[num1];
+
+        while (num1 >= 0) {
+            if (num1 > vals.length) {
+                System.out.print("\n " + num1);
+            }
+            num1--;
+        }
+
+    }
+
+}
