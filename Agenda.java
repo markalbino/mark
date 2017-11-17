29f18f912590a89725f647103a49574394fec05a
markalbino <32577581+markalbino@users.noreply.github.com>
2017-11-17 19:59:32 -0200

    Update Agenda.java

--- Agenda.java
+++ Agenda.java
@@ -141,3 +141,61 @@ public class Alterando {
 
 
 
+    \\Deletando :
+
+
+package deletando;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+package deletando;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Deletando {
+
+    public static Connection conexao;
+    public static PreparedStatement ps;
+    public static ResultSet rs;
+    public static String sql;
+
+    public static void main(String[] args) throws SQLException {
+
+        String usuario = "root";
+        String senha = "";
+        conexao = DriverManager.getConnection("jdbc:mysql://localhost:3306/cadastro", usuario, senha);
+        sql = "DELETE FROM Agenda where id=1";
+        ps = conexao.prepareStatement(sql);
+
+        ps.execute();
+        ps.close();
+        conexao.close();
+
+        System.out.println("\n\nListando os Registros Gravados");
+
+      
+        conexao = DriverManager.getConnection("jdbc:mysql://localhost:3306/cadastro", usuario, senha);
+        sql = "SELECT * FROM agenda ORDER BY nome";
+        ps = conexao.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+
+    }
+
+}
+
+
+
+
+
+
+
